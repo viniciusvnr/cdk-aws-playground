d3ef23325a4ad0cd9375f7f0ea8a74e57b464d38
viniciusvnr <viniciusvnr@gmail.com>
2019-11-13 23:43:04 -0300

    comment

--- cdk_aws_playground/cdk_aws_playground_stack.py
+++ cdk_aws_playground/cdk_aws_playground_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-
+from aws_cdk import ec2
 
 class CdkAwsPlaygroundStack(core.Stack):
 
@@ -8,6 +8,32 @@ class CdkAwsPlaygroundStack(core.Stack):
 
         # The code that defines your stack goes here
         # TODO: vpc
+        # subnetConfiguration: [
+        #     {
+        #     cidrMask: 24,
+        #     name: 'ingress',
+        #     subnetType: SubnetType.PUBLIC,
+        #     },
+        #     {
+        #     cidrMask: 24,
+        #     name: 'application',
+        #     subnetType: SubnetType.PRIVATE,
+        #     },
+        #     {
+        #     cidrMask: 28,
+        #     name: 'rds',
+        #     subnetType: SubnetType.ISOLATED,
+        #     }
+        #  ]
+        
+        vpc_main = ec2.Vpc(self, 'mainvpc-pg',
+            cidr='10.0.0.0/16',
+            
+        )
+
+
+
+
         # TODO: sagemaker
         # TODO: lambda
         # TODO: api gateway
